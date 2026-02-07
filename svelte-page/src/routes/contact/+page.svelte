cdf22193e948f56b3ac12e08d584f5254565bb2b
jack-baselayer <259399832+jack-baselayer@users.noreply.github.com>
2026-02-07 01:05:57 -0500

    Add .env for template vars.
    Update contact page

---
+++ svelte-page/src/routes/contact/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { PUBLIC_EMAIL, PUBLIC_RESPONSE_TIME } from "$env/static/public";
+</script>
+
+<h1>Contact</h1>
+
+<p>
+  <strong>Email:</strong>
+  <a href="mailto:{PUBLIC_EMAIL}">{PUBLIC_EMAIL}</a>
+</p>
+
+<h2>Best for:</h2>
+<ul>
+  <li>Information requests</li>
+  <li>Project engagement</li>
+  <li>Technical consulting</li>
+</ul>
+
+<p><strong>Response:</strong> {PUBLIC_RESPONSE_TIME}</p>
