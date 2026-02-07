5a39b9685854af1130514725136e806c41e09d0a
jack-baselayer <259399832+jack-baselayer@users.noreply.github.com>
2026-02-07 00:47:32 -0500

    Add about page, nav, set prerender

--- svelte-page/src/routes/+page.svelte
+++ svelte-page/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+</nav>
+
+<h1>Base Layer Consulting LLC</h1>
+<p>Technology services consulting.</p>
---
+++ svelte-page/src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<nav>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+</nav>
+
+<h1>About Base Layer LLC</h1>
+<p>Base Layer is an Infrastructure and DevOps consulting LLC.</p>
